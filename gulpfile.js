8f103a32643746e85efb250c33aa8a7760924cff
Bobby Johnson <popcorn245@gmail.com>
2015-09-03 21:02:29 -0500

    Move paths to config file, fix watchers, notify browser of errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 // REQUIRE DEPENDENCIES
 var gulp        = require('gulp'),
+    gulpif      = require('gulp-if'),
+    fs          = require('fs'),
     browserSync = require('browser-sync').create(),
     bower       = require('gulp-bower'),
-    reload      = browserSync.reload,
     concat      = require('gulp-concat'),
     cssmin      = require('gulp-minify-css'),
     plumber     = require('gulp-plumber'),
@@ -12,64 +13,60 @@ var gulp        = require('gulp'),
     uglify      = require('gulp-uglify');
 
 // VARIABLES ( PUT INTO JSON FILE EVENTUALLY )
-var paths = {
-    // These are your source files that will become the built files
-    source: {
-        styles: 'src/scss/main.scss',
-        // This controls the order it is concatenated into the final app.js file
-        js: [ 
-            'bower_components/jquery/dist/jquery.js',
-            'src/js/pages/*.js',
-            'src/js/app.js'
-        ]
-    },
-    // This is where the production code lives
-    destination: {
-        styles: 'www/css',
-        js: 'www/js',
-        html: 'www'
-    },
-    // This is where we are watching for changes
-    watch: {
-        styles: 'src/scss/*.scss',
-        js:'src/js/*.js',
-        html: 'www/*.html'
-    }
-};
-// This is how long the error messages with pop up in the browser
-var errorTimeout = 10000;
+function setVars(){
+    // Put the global vars inside of a function so you
+    // could call a refresh on them with a watch task.
+    configFile   = 'config.json';
+    configJSON   = JSON.parse(fs.readFileSync(configFile));
+    paths = configJSON.paths;
+    errorTimeout = 10000; // How long the error messages with pop up in the browser
+}
+setVars();
 
 // TASKS
 gulp.task('bower', function(){
+    // Runs bower install so you don't have to
     return bower();
 });
 
-gulp.task('install', function(){ // Installs Bower, Runs Build, and Starts Browser Sync
+gulp.task('install', function(){ 
+    // Installs Bower, Runs Build, and Starts Browser Sync
+    
     // This is a LIFE SAVER. It runs task in sequence.
     // So it waits for one to complete before moving to the next
     // Make sure each of your tasks returns something or it won't wait
     runSequence('bower', 'default');
 });
 
-gulp.task('serve', function(){ // Start Browser Sync
+
+gulp.task('sync', function(){ 
+    // Start Browser Sync and open Browser
     browserSync.init({
-        server:paths.destination.html
+        server: {
+            baseDir: paths.destination.html
+        }
     });
+
+    // Watch Files
+    gulp.watch(paths.watch.js, ['scripts']);
+    gulp.watch(paths.watch.styles, ['styles']);
+    gulp.watch(paths.watch.html, ['reload'])
 });
 
 gulp.task('reload', function(){
+    // Reload Browser
     browserSync.reload();
 });
 
 gulp.task('scripts', function(){ // Compiles and Minifies JavaScripts
+    var streamError = false;
     return gulp.src(paths.source.js)
         .pipe(plumber({
             // Plumber catches errors in the stream
             // Then I use Browser Sync to notify you of them 
             // in the browser
             errorHandler: function(error) {
-                console.log(error.message);
-                browserSync.notify(error.message, errorTimeout);
+                streamError = error;
                 this.emit('end');
             }
         }))
@@ -81,43 +78,44 @@ gulp.task('scripts', function(){ // Compiles and Minifies JavaScripts
         }))
         .pipe(gulp.dest(paths.destination.js))
     	.on('end', function(){
-            // This runs when task is finished
-            reload();
+            if(streamError){
+                // If there was an error then log it to console 
+                // and notify on browser
+                console.log(streamError.message);
+                browserSync.notify(streamError.message, errorTimeout);
+            }else{
+                // Reload Browser 
+                browserSync.reload();
+            }
         });
 });
 
 gulp.task('styles', function(){ // Compiles and Minifies Styles
+    var streamError = false;
     return gulp.src(paths.source.styles)
         .pipe(plumber({
             // Plumber catches errors in the stream
             // Then I use Browser Sync to notify you of them 
             // in the browser
             errorHandler: function(error) {
-                console.log(error.message);
-                browserSync.notify(error.message, errorTimeout);
+                streamError = error;
+                console.log(streamError.message);
+                browserSync.notify(streamError.message, errorTimeout);
                 this.emit('end');
             }
         }))
         .pipe(sass())
         .pipe(rename('app.css'))
         .pipe(gulp.dest(paths.destination.styles))
+        .pipe(gulpif(!streamError, browserSync.reload({stream:true})))
         .pipe(cssmin())
         .pipe(rename({
             suffix: '.min'
         }))
         .pipe(gulp.dest(paths.destination.styles))
-        .on('end', function(){
-            // This runs when task is finished
-            reload({stream:true});
-        });
-});
-
-gulp.task('watch',function(){ // Watches files for changes and runs tasks
-    gulp.watch(paths.source.js, ['scripts']);
-    gulp.watch(paths.source.scss, ['styles']);
-    gulp.watch(paths.source.html, ['reload']);
+        .pipe(gulpif(!streamError, browserSync.reload({stream:true})));        
 });
 
 gulp.task('default', function(){
-    runSequence('scripts', 'styles', 'serve', 'watch');
+    runSequence('scripts', 'styles', 'sync');
 });
